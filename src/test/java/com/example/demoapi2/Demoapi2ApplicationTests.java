16ca39b786c783a9c16af7da41da7b62d95fe4bb
Leonardo Baini <AR00122207@latam1.prosegur.local>
2019-10-22 21:01:16 -0300

    primer

---
+++ src/test/java/com/example/demoapi2/Demoapi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoapi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
